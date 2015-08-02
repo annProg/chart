28b583473c5da0048f2bba719b35e9c6976b8052
annprog <tecbbs@qq.com>
2015-08-02 15:41:31 +0800

    增加gnuplot画图。实现函数画图

--- api.php
+++ api.php
@@ -4,65 +4,148 @@
  *
  * $Id api.php  tecbbs@qq.com 2015-6-8 $
  **/
-$host = $_SERVER['HTTP_HOST'];
-$siteurl = "http://" . $host . "/gv/"; 
-$cht = "gv:dot";
-$chl = "graph {fontname=\"SimSun\";node[shape=box];a[label=\"nothing to do~ 参数错误\"];}";
-$error = "error.png";
-$engine = array("dot", "neato", "fdp", "sfdp", "twopi", "circo");
-$imgtype = "png"; 
-$imgtype_arr = array("png", "gif", "jpeg");
 
-if(isset($_GET['cht']) && isset($_GET['chl']) && isset($_GET['chof'])) {
-	$cht = $_GET['cht'];
-	$chl = $_GET['chl'];
-	$imgtype = $_GET['chof'];
-} elseif(isset($_POST['cht']) && isset($_POST['chl']) && isset($_POST['chof'])) {
-	$cht = $_POST['cht'];
-	$chl = urldecode($_POST['chl']);
-	$imgtype = $_POST['chof'];
-} else {
+$config = array();
+$config['engine'] = array("gv","gv:dot", "gv:neato", "gv:fdp", "gv:sfdp", "gv:twopi", "gv:circo", "gp");
+$config['imgtype'] = array("png", "gif", "jpeg");
+$config['out']['code'] = "./code/";
+$config['out']['img'] = "./img/";
+foreach ( $config['out'] as $v ) {
+	if(!file_exists($v)) {
+		mkdir($v, 0755);
+	} 
+}
+if(isset($_SERVER['HTTP_HOST'])) {
+	$config['host'] = $_SERVER['HTTP_HOST'];
+}
+else {
+	$config['host'] = "localhost";
+}
+$config['siteurl'] = "http://" . $config['host'] . "/gv/";
+$config['error'] = "error.png";
+
+
+function error() {
 	die("args error! nothing to do...");
 }
- 
-$arr = explode(':', $cht);
-if(!array_key_exists("1", $arr)) {
-	$cht = "dot";
-} else {
-	$cht = $arr['1'];
-}
-if(!in_array($cht, $engine)) {
-	$cht = "dot";
-}
 
-if(!in_array($imgtype, $imgtype_arr)) {
-	$imgtype = "png";
-}
-
-$gvname = md5($chl) . $cht;
-$gvpath = "./gv/" . $gvname . ".gv";
-$imgpath = "img/" . $gvname . "." .  $imgtype;
-
-
-$file = fopen("$gvpath", "w");
-$encode = mb_detect_encoding($chl, array("ASCII","UTF-8","GB2312", "GBK", "EUC-CN"));
-
-if($encode != "UTF-8") { 
-	$chl = iconv("$encode", "UTF-8", $chl);
-}
-
-$chl = str_replace("&gt;", ">", $chl);
-$chl = str_replace("&lt;", "<", $chl);
-$chl = str_replace("&quot;", "\"", $chl);
-fwrite($file, "$chl");
-fclose($file);
-if(!file_exists($imgpath)) {
-	exec("$cht -T$imgtype $gvpath -o $imgpath",$out, $ret);
-	if($ret != 0) {
-		$imgpath = $error;		
-		$imgtype = "png";
+function get_args() {
+	$ret = array();
+	global $config;
+	if(isset($_GET['cht']) && isset($_GET['chl'])) {
+		$ret['cht'] = $_GET['cht'];
+		$ret['chl'] = $_GET['chl'];
+		if(isset($_GET['chof']))
+			$ret['imgtype'] = $_GET['chof'];
+		else
+			$ret['imgtype'] = "png";
+	} elseif(isset($_POST['cht']) && isset($_POST['chl'])) {
+		$ret['cht'] = $_POST['cht'];
+		$ret['chl'] = urldecode($_POST['chl']);
+		
+		if(isset($_POST['chof']))
+			$ret['imgtype'] = $_POST['chof'];
+		else
+			$ret['imgtype'] = "png";
+	} else {
+		error();
 	}
+	if(!in_array($ret['cht'], $config['engine'])) {
+		error();
+	}
+	if(!in_array($ret['imgtype'], $config['imgtype'])) {
+		$ret['imgtype'] = "png";
+	}
+	$name = md5($ret['chl']) . ":" . $ret['cht'];
+	$ret['out']['code'] = $config['out']['code'] . $name;
+	$ret['out']['img'] = $config['out']['img'] . $name . "." . $ret['imgtype'];
+	return $ret;
 }
+
+function write_code($arr) {
+	$filepath = $arr['out']['code'];
+	$chl = $arr['chl'];
+
+	$file = fopen("$filepath", "w");
+	$encode = mb_detect_encoding($chl, array("ASCII","UTF-8","GB2312", "GBK", "EUC-CN"));
+
+	if($encode != "UTF-8") { 
+		$chl = iconv("$encode", "UTF-8", $chl);
+	}
+
+	$chl = str_replace("&gt;", ">", $chl);
+	$chl = str_replace("&lt;", "<", $chl);
+	$chl = str_replace("&quot;", "\"", $chl);
+	fwrite($file, "$chl");
+	fclose($file);
+}
+
+function graphviz($arr) {
+	$ret = array();
+	$imgpath = $arr['out']['img'];
+	$imgtype = $arr['imgtype'];
+
+	$gvpath = $arr['out']['code'];
+	$engine = $arr['engine'];
+	global $config;
+	
+	$ret['imgpath'] = $imgpath;
+	$ret['imgtype'] = $imgtype;
+
+	if(!file_exists($imgpath)) {
+		exec("$engine -T$imgtype $gvpath -o $imgpath", $out, $res);
+		if($res != 0) {
+			$ret['imgpath'] = $config['error'];
+			$ret['imgtype'] = "png";
+		}
+	}
+	
+	return $ret;
+}
+
+function gnuplot($arr) {
+	$ret = array();
+	$imgpath = $arr['out']['img'];
+	$imgtype = $arr['imgtype'];
+
+	$codepath = $arr['out']['code'];
+	$engine = $arr['engine'];
+	global $config;
+
+	$ret['imgpath'] = $imgpath;
+	$ret['imgtype'] = $imgtype;
+
+	if(!file_exists($imgpath)) {
+		exec("$engine -e \"set term png;set output '$imgpath'\" $codepath", $out, $res);
+		if($res != 0) {
+			$ret['imgpath'] = $config['error'];
+			$ret['imgtype'] = "png";
+		}
+	}
+	return $ret;
+}
+
+$api = get_args();
+write_code($api);
+
+$arr = explode(':', $api['cht']);
+switch ($arr[0]) {
+	case "gv":
+		if(array_key_exists("1", $arr)) {
+			$api['engine'] = $arr[1];
+		} else {
+			$api['engine'] = "dot";
+		}
+		$plot = graphviz($api);break;
+	case "gp":
+		$api['engine'] = "gnuplot";
+		$plot = gnuplot($api);break;
+	defualt:
+		error();
+}
+
+$imgtype = $plot['imgtype'];
+$imgpath = $plot['imgpath'];
 
 $imgstrout = "image$imgtype(imagecreatefrom$imgtype('$imgpath'));";
 if(isset($_GET['cht']) && isset($_GET['chl'])) {
