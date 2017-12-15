65052346b6d353fe6647e38a7cce9313855517e9
annprog <tecbbs@qq.com>
2017-12-15 12:02:46 +0800

    test.php使用ace编辑器

--- test.php
+++ test.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<script src="https://cdn.bootcss.com/ace/1.2.9/ace.js"></script>
+<script src="https://cdn.bootcss.com/ace/1.2.9/mode-dot.js"></script>
+<script src="https://cdn.bootcss.com/ace/1.2.9/mode-asciidoc.js"></script>
+<script src="https://cdn.bootcss.com/ace/1.2.9/theme-github.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
 <style type="text/css">
 body { 
@@ -12,9 +16,10 @@ body {
 textarea {
 	font-family: "Courier New";
 }
+
 #content {
 	box-shadow: 0 0 5px #BBB;
-	height: 1000px;
+	height: 650px;
 	background: #F7F7F7;
 	border: 1px solid #ccc; 
 	padding: 30px 100px 60px 10px;
@@ -24,6 +29,17 @@ textarea {
 	width: 49%;
 	float: left;
 }
+
+#ace_editor {
+	width: 45%;
+	position: absolute;
+	height: 500px;
+	cursor: text;
+	user-select: text;
+	box-shadow: 1px 1px 1px 1px #a8b4b9;	
+	-webkit-box-shadow: 1px 1px 1px 1px #a8b4b9;	
+	-moz-box-shadow: 1px 1px 1px 1px #a8b4b9;
+}
 #editor #code {
 	width: 100%;
 	height: 500px;
@@ -31,12 +47,16 @@ textarea {
 #preview {
 	width: 45%;
 	margin-left: 20px;
+	padding-left: 50px;
+	padding-top: 80px;
 	float: right;
+	text-align: center;
 }
 </style>
 <title>Chart Api Test Tool</title>
 </head>
 <body>
+
 <div id="content">
 <div id="editor">
 <form method="GET" role="form" class="form-horizontal">
@@ -88,5 +108,33 @@ if(isset($_GET['submit']))
 }
 ?>
 </div>
+<script>
+	var input = document.getElementById("code");
+	input.style.display="none";
+	var aceDiv = document.createElement('div');
+	aceDiv.id="ace_editor";
+	input.parentElement.parentElement.appendChild(aceDiv);
+
+	var engine = document.getElementById('engine');
+	var aceEditor = ace.edit("ace_editor");
+	engine.onchange = function () {
+		console.log(engine.value);
+		if(engine.value == "ditaa") {
+			aceEditor.getSession().setMode("ace/mode/asciidoc");
+		}else {
+			aceEditor.getSession().setMode("ace/mode/dot");
+		}
+
+	}
+	aceEditor.setTheme("ace/theme/github");
+	aceEditor.setValue(input.value);
+	aceEditor.setHighlightActiveLine(true);
+	aceEditor.getSession().setUseWrapMode(true);
+	aceEditor.setFontSize("18px");
+
+	aceEditor.getSession().on("change", function(e) {
+		input.value = aceEditor.getValue();
+	});
+</script>
 </body>
 </html>
