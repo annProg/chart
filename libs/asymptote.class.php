a4e6389749aa191ebc60d87dbfdc1838f01f7aa4
annprog <tecbbs@qq.com>
2018-03-07 18:21:50 +0800

    add Asymptote

---
+++ libs/asymptote.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Usage:
+ * File Name:
+ * Author: annhe  
+ * Mail: i@annhe.net
+ * Created Time: 2017-12-16 02:44:03
+ **/
+$config['engine']['asy'] = array(
+	"desc"=>"Asymptote",
+	"usage" => "Asymptote language. see http://asymptote.sourceforge.net/",
+	"class" => "asymptote"
+);
+
+class asymptote extends plot {
+	private $valid_chof = array("png","jpg","gif","pdf","eps");
+	function __construct($args) {
+		parent::__construct($args);
+		if(!in_array($this->chof, $this->valid_chof)) {
+			$this->chof = "png";
+		}
+	}
+
+	function render() {
+		$p = parent::render();
+		if($p) return($p);
+		exec("asy $this->ifile -f $this->chof -o $this->ofile", $out, $res);
+		if($res != 0) {
+			$this->onerr();
+		}
+		return $this->result();
+	}
+}
