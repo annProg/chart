0bedeb9a8a06ceaa0e07f4ca6e0a71fdea391023
annprog <tecbbs@qq.com>
2020-04-05 03:23:42 +0800

    fix: api link

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ if(isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) $scheme = $_SERVER['HTTP_X_FORWARD
 $config = array();
 // api链接地址
 $config['rooturl'] = $scheme . "://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']);
-$config['api'] = $config['rooturl'] . "/api.php";
+$config['api'] = $config['rooturl'] . "api.php";
 // engine类型
 $config['engine'] = array();
 // 禁用某些engine
